b6a50900a8374d7c1f9366fc8769b7e272d948c8
Ayaf-Bless <blessambel1@gmail.com>
2022-06-02 11:52:46 -0900

    style and create components

---
+++ frontend/components/layout/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-1">
+      <p className="text-center mt-1">
+        Jobbee - 2021-2022, All Rights Reserved
+        <a
+          className="ml-4"
+          rel="noreferrer"
+          target="_blank"
+          href="https://twitter.com/AmbeleBless"
+        >
+          Ayaf-Bless❤️‍
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
