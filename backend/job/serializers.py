3ff2aafca0784b8778bc87613b7b9832aa4b693c
Ayaf-Bless <blessambel1@gmail.com>
2022-06-02 06:51:42 -0900

    imolement user applying for jub

--- backend/job/serializers.py
+++ backend/job/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Job
+from .models import CandidatesApplied, Job
 
 
 class JobSerializer(serializers.ModelSerializer):
@@ -9,3 +9,12 @@ class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = "__all__"
+
+
+class CandidatesAppliedSerializer(serializers.ModelSerializer):
+
+    job = JobSerializer()
+
+    class Meta:
+        model = CandidatesApplied
+        fields = ('user', 'resume', 'appliedAt', 'job')
